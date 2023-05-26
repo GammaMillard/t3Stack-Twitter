fc204512d03faf481dd27dd39c7c593edfef37ad
Gammamillard <millard@gammatech.school>
2023-05-26 17:35:11 +0200

    Add emojis

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware();
-console.log('HOla');
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const user = useUser()
  
+  const {data} = api.posts.getAll.useQuery()
+  console.log(data);
   
   return (
     <>
     {
       user.isSignedIn && <SignOutButton /> 
     }
+    <div>
+      {data?.map(e => {
+        return <div key={e.id}> {e.content}</div>
+      })}
+    </div>
     </>
   );
 };
